cb51508188582a4e24f912b147dd82144489f2c1
dominikus1993 <dominikus1910@gmail.com>
2019-05-23 18:43:59 +0000

    add first struct

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
+#[derive(Debug)]
+struct CustomerBasket {
+    id: String
+}
+
 fn main() {
-    let a = 2;
-    println!("Hello, world! {}", a);
+    let id = String::from("dshjdsaguhdsahgds");
+    let basket = CustomerBasket { id };
+    println!("b = {:?}", basket)
 }
