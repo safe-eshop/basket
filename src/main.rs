b6fd32eafc9bfe0bd86230e1807b1a48f42e69b1
dominikus1993 <dominikus1910@gmail.com>
2019-05-29 21:12:56 +0000

    add baksetitem struct

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
+
+type ProductId = String;
+
+#[derive(Debug)]
+struct BasketItem {
+    id: ProductId,
+    quantity: u8
+}
+
+type CustomerId = String;
+
 #[derive(Debug)]
 struct CustomerBasket {
-    id: String
+    id: CustomerId,
+    items: Vec<BasketItem>
 }
 
 fn main() {
-    let id = String::from("dshjdsaguhdsahgds");
-    let basket = CustomerBasket { id };
+    let item = BasketItem { id: String::from("test"), quantity: 3 };
+    let basket = CustomerBasket { id: String::from("dshjdsaguhdsahgds"), items: vec![item] };
     println!("b = {:?}", basket)
 }
