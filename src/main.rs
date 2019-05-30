9e8b0c2c5a3cb6e409219e77402c394f74b23e37
dominikus1993 <dominikus1910@gmail.com>
2019-05-30 21:35:37 +0000

    small experiments

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,37 @@ struct CustomerBasket {
     id: CustomerId,
     items: Vec<BasketItem>
 }
+ 
+trait Basket {
+    fn add_item(&mut self, product_id: ProductId, quantity: u8);
+}
+
+impl CustomerBasket {
+    fn new(id: String) -> CustomerBasket {
+        return CustomerBasket{ id: id, items: Vec::new()}
+    }
+}
+
+impl Basket for CustomerBasket {
+    fn add_item(&mut self, product_id: ProductId, quantity: u8)  {
+        let items: Vec<_> = self.items.iter().filter(|&item| item.id == product_id).collect();
+        let item = items.first();
+        println!("{:?}", item)
+    }
+}
+
+fn get_customer_basket(customer_id: String) -> Result<CustomerBasket, String> {
+    let item = BasketItem { id: String::from("test"), quantity: 3 };
+    return Ok(CustomerBasket { id: customer_id, items: vec![item] });
+}
 
 fn main() {
-    let item = BasketItem { id: String::from("test"), quantity: 3 };
-    let basket = CustomerBasket { id: String::from("dshjdsaguhdsahgds"), items: vec![item] };
-    println!("b = {:?}", basket)
+    let basket = get_customer_basket(String::from("ghasdggadsjhgds"));
+    match basket {
+        Ok(mut b) => {
+            b.add_item(String::from("test"), 12);
+            println!("Ok {:?}", b);
+        }           
+        Err(er) =>  println!("Error {}", er)
+    }
 }
