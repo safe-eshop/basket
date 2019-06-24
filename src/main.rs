90e644e690cf3357bb0fe969cf9b3a536ac33b99
dominikus1993 <dominikus1910@gmail.com>
2019-06-24 20:52:56 +0000

    add redis

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
+extern crate redis;
+use redis::Commands;
 use actix_web::{web, middleware, App, HttpServer, Responder};
 
 fn index(info: web::Path<String>) -> impl Responder {
-    format!("Hello {}!", *info)
+    let client = redis::Client::open("redis://redis/").unwrap();
+    let con = client.get_connection().unwrap();
+    // throw away the result, just make sure it does not fail
+    let _ : () = con.set("my_key", 42).unwrap();
+    // read back the key and return it.  Because the return value
+    // from the function is a result for integer this will automatically
+    // convert into one.
+    let a = con.get("my_key").unwrap_or(0i32);
+    format!("Hello {}! id from redis = {}", *info, a)
 }
 
 fn main() -> std::io::Result<()> {
