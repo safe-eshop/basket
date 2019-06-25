a0b43731cbffa61c175d65390166e4d459809d01
Dominik.Kotecki <dominik.kotecki@rossmann.com.pl>
2019-06-25 14:46:42 +0000

    small changes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 extern crate redis;
 use redis::Commands;
-use actix_web::{web, middleware, App, HttpServer, Responder};
+use actix_web::{web, middleware, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+fn redis() -> redis::Connection {
+    let client = redis::Client::open("redis://redis/").unwrap();
+    return client.get_connection().unwrap();
+}
 
 fn index(info: web::Path<String>) -> impl Responder {
     let client = redis::Client::open("redis://redis/").unwrap();
@@ -14,6 +19,21 @@ fn index(info: web::Path<String>) -> impl Responder {
     format!("Hello {}! id from redis = {}", *info, a)
 }
 
+fn get_customer_basket() -> HttpResponse  {
+    let con = redis();
+    // convert into one.
+    let customer_basket: Result<Option<String>, redis::RedisError> = con.get("4cba04f1-2436-4d79-b184-b1a8521ff0f9");
+    match customer_basket {
+        Ok(basket) => {
+            HttpResponse::Ok().json(2)
+        }
+        Err(ex) => {
+            println!("Error {}", ex);
+            HttpResponse::Ok().json(1)
+        }
+    }   
+}
+
 fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
@@ -23,7 +43,8 @@ fn main() -> std::io::Result<()> {
         || App::new()
         .wrap(middleware::Logger::default())
         .service(
-              web::resource("/hello/{name}").to(index)))
+              web::resource("/hello/{name}").to(index))
+        .service(web::resource("/").to(get_customer_basket)))
         .bind("127.0.0.1:8080")?
         .start();
     println!("Starting http server: 127.0.0.1:8080");
