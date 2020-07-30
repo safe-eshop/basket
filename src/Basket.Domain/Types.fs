71966ad4eadae014b3bf82c6d9e6379f2506102f
dominikus1993 <dominikus1910@gmail.com>
2020-07-30 14:40:50 +0200

    add Domian

---
+++ src/Basket.Domain/Types.fs
@@ -0,0 +1,14 @@
+﻿namespace Basket.Domain.Types
+
+open System
+
+type ItemId = Guid
+type Quantity = int
+type CustomerId = Guid
+
+type Item = { Id: ItemId; Quantity: Quantity }
+type Items = Item seq
+
+type CustomerBasket = { CustomerId: CustomerId; Items: Items }
+
+type BasketError = int
